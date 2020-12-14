cb34276a0abfa55424296d22bdcc3bcecdde5890
Kavya Krishnan <72289432+krishkavya@users.noreply.github.com>
2020-12-14 14:16:47 +0530

    Update euler-2.cpp

--- Task-15/euler-2.cpp
+++ Task-15/euler-2.cpp
@@ -1,28 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-    int t,s;
-    long  N;
-     cin >> t;
-     long A[100];
-     A[0]=1;
-     A[1]=2;
-     for(int i=2;i<100;i++){
-         A[i]=A[i-1]+A[i-2];
-     }
-    for(int a0 = 0; a0 < t; a0++)
-    {   s=0;  
-        cin>>N;
-        for(int i=1;i<100;i++){
-          if(A[i]%2==0 && A[i]<N)
-          {
-              s+=A[i];
-          }else if(A[i]>N){
-              break;
-          }
-      }
-        cout<<s<<endl;
+    unsigned int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        unsigned long long n;
+        cin >> n;
+        unsigned long long sum=0;
+        unsigned long long a=1;
+        unsigned long long b=2;
+        while(b<=n){
+            if(b%2==0){
+                sum=sum+b;
+            }
+            auto next=a+b;
+            a=b;
+            b=next;
+        }
+        cout<<sum<<endl;
     }
-   return 0;
+    return 0;
 }
